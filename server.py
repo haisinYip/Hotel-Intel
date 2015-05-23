b43e280663bacf45793726567baf063602076ae3
root <anna.vassilovski@gmail.com>
2015-05-23 19:26:36 -0400

    fixed static paths

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ from flask import Flask, session, request, render_template, flash, redirect, url
 from db_data_extractor import *
 from api_data_extractor import *
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 @app.route("/", methods=["GET"])
 def renderMainPage():
