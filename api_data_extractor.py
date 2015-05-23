7a564261e08ef59749b930d70e7c047ccda98ab9
Haisin <hai.yip@mail.mcgill.ca>
2015-05-23 12:17:17 -0400

    added api data extractor module

---
+++ api_data_extractor.py
@@ -0,0 +1,20 @@
+# generate urls for different queries and return relevant data
+
+EXPEDIA_API_KEY = '2lV731SuJdeTFSvmWHR9LBMJAZsPc6e2'
+YELP_API_KEY = 
+
+EXPEDIA_BASE_URL = 'http://terminal2.expedia.com/x/suggestions/'
+YELP_BASE_URL = 
+
+# compute avg location
+def getAvgLocation(location_1, location_2, location_3):
+	return "not implemented"
+
+# return hotels in avg location 
+def getHotelsInAvgLocation(avg_location, date):
+	return "not implemented"
+
+if __name__ == "__main__":
+    print "test functions"
+ 
+
