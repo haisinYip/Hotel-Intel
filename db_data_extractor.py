37ebae766b73a156eb56e6415dca4b9aa58b0ef6
m3rl10n <anna.vassilovski@gmail.com>
2015-05-23 17:32:45 -0400

    added query and packaged result set

--- db_data_extractor.py
+++ db_data_extractor.py
@@ -1,15 +1,27 @@
 import mysql.connector
 from mysql.connector import Error
 
-def getDataFromDB(query):
-
+def getLocationDataFromDB():
+    query = """select appcategory, app_categories.name as category, yelp_categories.name as tag, yelp_locations.* from (
+            select 
+            yelp_loc_to_yelp_cat.yelp_loc_id as location, 
+            yelp_loc_to_app_cat.app_cat_id as appcategory, 
+            yelp_loc_to_yelp_cat.yelp_cat_id as yelpcategory,
+            count(yelp_loc_to_yelp_cat.yelp_cat_id) as yelpcount
+            from yelp_loc_to_app_cat 
+            left join yelp_loc_to_yelp_cat on yelp_loc_to_yelp_cat.yelp_loc_id=yelp_loc_to_app_cat.yelp_loc_id
+            group by yelp_loc_to_yelp_cat.yelp_cat_id
+            ) as source_locations
+            left join yelp_locations on yelp_locations.id=location
+            left join yelp_categories on yelp_categories.id=yelpcategory
+            left join app_categories on app_categories.id=appcategory
+            order by appcategory, yelpcount desc"""
     data = []
-
     try:
-        conn = mysql.connector.connect(host='localhost',
+        conn = mysql.connector.connect(host='45.55.140.88',
                                        database='hotel_intel_db',
-                                       user='root',
-                                       password='foobar')
+                                       user='anna',
+                                       password='')
         if conn.is_connected():
             print('Connected to MySQL database')
 
@@ -31,7 +43,13 @@ def getDataFromDB(query):
         conn.close()
         return data
  
+def getLocationData(data):
+    results = []
+    for result in data:
+        categoryid, categoryname, yelptag, locationid, yelpid, name, url, img_url, lat, lon = result
+        results.append({'categoryid' : categoryid, 'categoryname' : categoryname, 'yelptag' : yelptag, 'locationid' : locationid, 'name' : name, 'url' : url, 'img_url' : img_url, 'lat' : lat, 'lon' : lon})
+    return results
+
 if __name__ == '__main__':
-    query = "select yelp_locations.id,yelp_loc_to_yelp_cat.yelp_loc_id, count(*) from yelp_locations,yelp_loc_to_yelp_cat where yelp_locations.id=yelp_loc_to_yelp_cat.yelp_loc_id group by yelp_locations.id order by count(*) DESC LIMIT 5"
-    d = getDataFromDB(query)
-    print d
+    dbdata = getLocationDataFromDB()
+    locationdata = getLocationData(dbdata)
